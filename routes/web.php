7c72ebdf4c049eb92da90d73e642e9088b96bcbb
giannurwana19 <giannurwana19@gmail.com>
2021-12-19 17:42:07 +0700

    Struktuk folder, routes dan view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Gian Nurwana',
+        'email' => 'giannurwana19@gmail.com',
+        'image' => 'avatar.png'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
