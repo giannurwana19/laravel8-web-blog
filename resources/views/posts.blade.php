2c6015d04a2055f350e611b8abbb4d4ff0f49db3
giannurwana19 <giannurwana19@gmail.com>
2021-12-19 19:05:43 +0700

    Blade templating engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+{{-- @dd($posts) --}}
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>My Blog | Blog</title>
-</head>
-
-<body>
-    <h1>Halaman Blog</h1>
-</body>
-
-</html>
+@section('container')
+@foreach ($posts as $post)
+<article class="mb-3">
+    <h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
+    <h5>By: {{ $post['author'] }}</h5>
+    <p>{{ $post['body'] }}</p>
+</article>
+@endforeach
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        'title' => 'About',
         'name' => 'Gian Nurwana',
         'email' => 'giannurwana19@gmail.com',
         'image' => 'avatar.png'
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('posts');
+$blogPosts = [
+    [
+        'title' => 'Judul Post Pertama',
+        'slug' => 'judul-post-pertama',
+        'author' => 'Gian',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci!'
+    ],
+    [
+        'title' => 'Judul Post Kedua',
+        'slug' => 'judul-post-kedua',
+        'author' => 'Andi',
+        'body' => 'soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci!'
+    ],
+    [
+        'title' => 'Judul Post Pertama',
+        'slug' => 'judul-post-ketiga',
+        'author' => 'Lukman',
+        'body' => 'quibusdam? Voluptate Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci! dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis q'
+    ],
+];
+
+Route::get('/blog', function () use ($blogPosts) {
+    return view('posts', ['title' => 'Blog', 'posts' => $blogPosts]);
+});
+
+
+Route::get('/posts/{slug}', function ($slug) use ($blogPosts) {
+    $newPost = [];
+    foreach ($blogPosts as $post) {
+        if ($post['slug'] === $slug) {
+            $newPost = $post;
+        }
+    }
+
+    return view('post', ['title' => 'Single Post', 'post' => $newPost]);
 });
