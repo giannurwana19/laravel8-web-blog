759c333e582f47be000e3c550b1e1b13e3842e01
giannurwana19 <giannurwana19@gmail.com>
2021-12-20 19:12:09 +0700

    Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,8 +12,8 @@ class PostController extends Controller
         return view('posts', ['title' => 'Posts', 'posts' => Post::all()]);
     }
 
-    public function show($slug)
+    public function show(Post $post)
     {
-        return view('post', ['title' => 'Single Post', 'post' => Post::find($slug)]);
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,38 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blogPosts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Gian',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci!'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Andi',
-            'body' => 'soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci!'
-        ],
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-ketiga',
-            'author' => 'Lukman',
-            'body' => 'quibusdam? Voluptate Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis quibusdam? Voluptate harum dolorum expedita facilis quo adipisci! dolor sit, amet consectetur adipisicing elit. Eum, corporis cumque itaque culpa nisi quis voluptatem neque tenetur repellendus, nulla mollitia. Perferendis quasi cumque tempore natus ex nostrum illo, soluta asperiores rem. Beatae, sequi placeat a impedit consequuntur voluptatem eaque fuga reiciendis q'
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blogPosts);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
 
-    public static function find($slug)
-    {
-        $posts = static::all();
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
 }
