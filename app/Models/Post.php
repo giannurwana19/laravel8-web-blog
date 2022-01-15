cbe06d2263de3d0087cc7e6bbfd8e82a74d7a397
giannurwana19 <giannurwana19@gmail.com>
2022-01-16 05:43:51 +0700

    Searching & pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,6 +20,27 @@ class Post extends Model
      */
     protected $guarded = ['id'];
 
+    public function scopeFilter($query, array $filters)
+    {
+        // if ($filters['search'] ?? false) {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')->orWhere('body', 'like', '%' . $filters['search'] . '%');
+        // }
+
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        $query->when($filters['author'] ?? false, fn ($query, $author) =>
+        $query->whereHas('author', fn ($query) =>
+        $query->where('username', $author)));
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row mb-3 justify-content-center">
+    <div class="col-md-6">
+        <form action="/posts">
+            @if(request('category'))
+            <input type="hidden" name="category" value="{{ request('category') }}">
+            @endif
+            @if(request('author'))
+            <input type="hidden" name="author" value="{{ request('author') }}">
+            @endif
+            <div class="input-group mb-3">
+                <input type="text" name="search" value="{{ request('search') }}" class="form-control"
+                    placeholder="Search">
+                <button class="btn btn-primary" type="submit">Search</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 @if($posts->count())
 <div class="card mb-3">
@@ -12,9 +30,9 @@
         </a>
         <p>
             <small>
-                By: <a href="/authors/{{ $posts[0]->author->username }}"
+                By: <a href="/posts?author={{ $posts[0]->author->username }}"
                     class="text-decoration-none">{{ $posts[0]->author->name }}</a>
-                in <a href="/categories/{{ $posts[0]->category->slug }}"
+                in <a href="/posts?category={{ $posts[0]->category->slug }}"
                     class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                 {{ $posts[0]->created_at->diffForHumans() }}
             </small>
@@ -25,9 +43,6 @@
         <a href="/posts/{{ $posts[0]->slug }}" class="btn btn-primary">Read more</a>
     </div>
 </div>
-@else
-<p class="fs-4">No Post found</p>
-@endif
 
 <div class="container">
     <div class="row">
@@ -35,15 +50,16 @@
         <div class="col-md-4 mb-3">
             <div class="card">
                 <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)">
-                    <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a>
+                    <a href="/posts?category={{ $post->category->slug }}"
+                        class="text-white text-decoration-none">{{ $post->category->name }}</a>
                 </div>
                 <img src="https://source.unsplash.com/500x300?{{ $post->category->name }}" class="card-img-top"
                     alt="{{ $post->category->name }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $post->title }}</h5>
-                    <p>s
+                    <p>
                         <small>
-                            By: <a href="/authors/{{ $post->author->username }}"
+                            By: <a href="posts?author={{ $post->author->username }}"
                                 class="text-decoration-none">{{ $post->author->name }}</a>
                             {{ $post->created_at->diffForHumans() }}
                         </small>
@@ -55,6 +71,14 @@
         </div>
         @endforeach
     </div>
+</div>
+
+@else
+<p class="fs-4 text-center">No Post found</p>
+@endif
+
+<div class="d-flex justify-content-center">
+    {{ $posts->links() }}
 </div>
 
 @endsection
