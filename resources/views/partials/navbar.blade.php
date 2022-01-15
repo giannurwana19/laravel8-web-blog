dd3ddf841917ea73227dbaaf2fd9d4d5c273b9f8
giannurwana19 <giannurwana19@gmail.com>
2022-01-16 06:13:28 +0700

    View login & registration

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,6 +21,13 @@
                         href="/categories">Categories</a>
                 </li>
             </ul>
+
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/login" class="nav-link{{ request()->is('login') ? ' active' : '' }}"><i
+                            class="bi bi-box-arrow-in-right"></i> Login</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
