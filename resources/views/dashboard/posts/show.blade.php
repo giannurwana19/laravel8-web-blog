2b62a0c334732e21d933eb982fba06ab5ca3cbe5
giannurwana19 <giannurwana19@gmail.com>
2022-01-19 20:00:10 +0700

    Dashboard UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,27 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-lg-8">
+            <h1 class="my-3">{{ $post->title }}</h1>
+
+            <a href="/dashboard/posts" class="btn btn-success btn-sm"><span data-feather="arrow-left"></span> Back to
+                all my
+                posts</a>
+            <a href="#" class="btn btn-warning btn-sm"><span data-feather="edit"></span> Edit</a>
+            <a href="#" class="btn btn-danger btn-sm"><span data-feather="x-circle"></span> Delete</a>
+
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3"
+                alt="{{ $post->category->name }}">
+
+            <article class="my-3 fs-5">
+                {!! $post->body !!}
+            </article>
+
+            <a href="/posts">Back to posts</a>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -48,7 +48,9 @@ Route::delete('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', fn () => view('dashboard.index'))->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
